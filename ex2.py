03d7c6053d39f723f88a720a97dfeceae3eddb23
lou <lou.goossens03@gmail.com>
2026-02-01 16:47:10 +0100

    first few exercises

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# A comment, everything after an octothorpe is ignored
+
+print("I could have code likt this.") # and the comment after is ignored
+
+# print("This won't run.")
+
+print("This will run.")
